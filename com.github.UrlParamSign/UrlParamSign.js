e0e3f3447ee3736d4967a33db9b1a7bdbb2ee14e
gently <gently@live.cn>
2019-10-28 18:54:09 +0800

    更新算法，完善
    完善url编码部分的内容

--- com.github.UrlParamSign/UrlParamSign.js
+++ com.github.UrlParamSign/UrlParamSign.js
@@ -3,25 +3,27 @@ const UrlParamSign = function() {
   // implement the evaluate() method to generate the dynamic value
   this.evaluate = function(ctx) {
     const signKey = ctx.getEnvironmentVariableByName("signKey").getCurrentValue();
-  	const req = ctx.getCurrentRequest();
+    const req = ctx.getCurrentRequest();
     const queryString = req.getUrlParametersNames().filter(function(k) { return k[0] !== "_" }).sort().map(function(k) {
-      return `${k}=${req.getUrlParameterByName(k)}`;
-    }).join("&");
+        let qs1 = req.getUrlParameterByName(k);
+        qs1 = qs1.replace(/([^\*\'.~!a-z0-9])/g, function($1){return "%" + $1.charCodeAt(0).toString(16).toUpperCase()});
+        return `${k}=${qs1}`;
+      }).join("&");
 
-    console.log(queryString);
+      console.log(queryString);
 
-    const dynamicValue = DynamicValue('com.luckymarmot.HashDynamicValue', {
+      const dynamicValue = DynamicValue('com.luckymarmot.HashDynamicValue', {
         'input': `${signKey}${queryString}${signKey}`,
         'hashType': 5
-    });
-    return dynamicValue.getEvaluatedString();
+      });
+      return dynamicValue.getEvaluatedString();
+    }
   }
-}
-// set the Extension Identifier (must be same as the directory name)
-UrlParamSign.identifier = "com.github.UrlParamSign";
-// give a display name to your Dynamic Value
-UrlParamSign.title = "UrlParamSign";
-// link to the Dynamic Value documentation
-UrlParamSign.help = "https://github.com/gentlyxu/UrlParamSign";
-// call to register function is required
-registerDynamicValueClass(UrlParamSign);
+  // set the Extension Identifier (must be same as the directory name)
+  UrlParamSign.identifier = "com.github.UrlParamSign";
+  // give a display name to your Dynamic Value
+  UrlParamSign.title = "UrlParamSign";
+  // link to the Dynamic Value documentation
+  UrlParamSign.help = "https://github.com/gentlyxu/UrlParamSign";
+  // call to register function is required
+  registerDynamicValueClass(UrlParamSign);
